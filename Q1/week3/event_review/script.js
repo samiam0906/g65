f69bfda8026d42db94c0d497a713021349c88425
Sam Hoffman <samdhoffman@gmail.com>
2017-09-05 11:08:00 -0400

    Add Event Listener Review

---
+++ Q1/week3/event_review/script.js
@@ -0,0 +1,16 @@
+document.querySelector("#red").addEventListener('click', function() {
+  // change background-color of the body
+  document.querySelector("body").style.backgroundColor = "red";
+})
+
+document.querySelector("#blue").addEventListener('click', function() {
+  document.querySelector("body").style.backgroundColor = "blue";
+})
+
+document.querySelector("#yellow").addEventListener('click', function() {
+  document.querySelector("body").style.backgroundColor = "yellow";
+})
+
+document.querySelector("#green").addEventListener('click', function() {
+  document.querySelector("body").style.backgroundColor = "green";
+})
